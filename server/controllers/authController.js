aba0e0e37adb4b47f5da4649ae193217a57874ae
hetianyuanjames <hty1016@ad.unc.edu>
2022-04-21 00:21:34 -0400

    updated auth

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+
 // controller actions
 module.exports.register_get = (req, res) => {
     res.render('Register');
@@ -8,26 +10,27 @@ module.exports.register_get = (req, res) => {
   }
   
   module.exports.register_post = (req, res) => {
-    const {username, password} = req.body;
-
     const user = req.app.get('db').collection('user');
-    
-    if (scheme.validate_user(req.body)) {
-      const data = {
-          user_name: username,
-          user_password: password
-      };
 
-      user.insertOne(data, (err, resdb) => {
-          if (err) res.send({
-              status: 'error',
-              debug: resdb
-          });
-          else res.send({
-              status: 'sucess',
-              result: resdb
-          });
-      });
+    if (scheme.validate_user(req.body)) {
+        const data = {
+            user_name: req.body.user_name,
+            user_email: req.body.user_email,
+            email_verified: true,
+            user_info: req.body.user_info,
+            user_intro: "This user doesn't create an introduction."
+        };
+
+        user.insertOne(data, (err, resdb) => {
+            if (err) res.send({
+                status: 'error',
+                debug: resdb
+            });
+            else res.send({
+                status: 'sucess',
+                result: resdb
+            });
+        });
     }
   }
   
