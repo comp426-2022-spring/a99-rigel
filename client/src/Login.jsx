877c4441e9782047e3bdb1f98199a42fb1357e7a
Peter Yao <69826380+pyao2212@users.noreply.github.com>
2022-04-29 19:15:51 -0400

    login

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,28 +1,20 @@
 import React, { Component } from "react";
 import { Redirect, Link } from "react-router-dom";
-import axios from "axios";
+import axios from 'axios';
 import "./styles/Login.css";
 
 class Login extends Component {
-    constructor(props) {
-        //Constructor, initialize the state of the Login component to have 2 fields:
-        super(props); //user_id, and user_password
-        this.state = {
-            islogged: false,
-            loginParams: {
-                user_id: "",
-                user_password: "",
-            },
-        };
-    }
+  constructor(props) { //Constructor, initialize the state of the Login component to have 2 fields:
+    super(props);      //user_id, and user_password
+    this.state = {
+      islogged: false,
+      loginParams: {
+        user_id: "",
+        user_password: ""
+      }
+    };
+  }
 
-<<<<<<< HEAD
-    handleFormChange = (event) => {
-        //Updates the user_id field or user_password field any time the user types
-        let loginParamsNew = { ...this.state.loginParams };
-        let val = event.target.value;
-        loginParamsNew[event.target.name] = val;
-=======
   handleFormChange = event => { //Updates the user_id field or user_password field any time the user types
     let loginParamsNew = { ...this.state.loginParams };
     let val = event.target.value;
@@ -46,84 +38,58 @@ class Login extends Component {
         localStorage.setItem("username", username); 
         localStorage.setItem("password", user_password);
         localStorage.setItem("user_id", response.user);
->>>>>>> a7ecbba9d81a262c93999306854971bbf308f074
         this.setState({
-            loginParams: loginParamsNew,
+          islogged: true
         });
-    };
+      }
+      else {
+        alert(JSON.stringify(response))
+        alert("Error: Invalid input")
+      }
+    })
+    .catch(function (error) {
+      alert(JSON.stringify(error))
+      alert("Error with HTTP request");
+    });
+    event.preventDefault();
+  };
 
-    login = (event) => {
-        //Post request to server containing the current user_id and user_password fields
-        const username = this.state.loginParams.user_id;
-        const user_password = this.state.loginParams.user_password;
-        axios
-            .post("http://localhost:5000/login", {
-                user_name: username,
-                password: user_password,
-            })
-            .then((response) => {
-                alert(JSON.stringify(response));
-                if (response.data.status === 1) {
-                    //We store info about the current user locally in browser
-                    localStorage.setItem("token", "T");
-                    localStorage.setItem("username", username);
-                    localStorage.setItem("password", user_password);
-                    localStorage.setItem("user_id", "5");
-                    this.setState({
-                        islogged: true,
-                    });
-                } else {
-                    alert(JSON.stringify(response));
-                    alert("Error: Invalid input");
-                }
-            })
-            .catch(function (error) {
-                alert(JSON.stringify(error));
-                alert("Error with HTTP request");
-            });
-        event.preventDefault();
-    };
-
-    render() {
-        //Render component
-        if (localStorage.getItem("token")) {
-            return <Redirect to="/" />;
-        }
-        return (
-            <div>
-                <br />
-                <div className="container">
-                    <form onSubmit={this.login} className="form-signin">
-                        <h1 className="h3 mb-3 font-weight-normal">
-                            Please sign in
-                        </h1>
-                        <div className="row">
-                            <div className="col">
-                                <input
-                                    type="text"
-                                    name="user_id"
-                                    onChange={this.handleFormChange}
-                                    placeholder="Enter Username"
-                                />
-                                <input
-                                    type="password"
-                                    name="user_password"
-                                    onChange={this.handleFormChange}
-                                    placeholder="Enter Password"
-                                />
-                                <input type="submit" value="Login" />
-                                <Link to="/register">
-                                    <button>
-                                        Register Here if You Don't Have an
-                                        Account!
-                                    </button>
-                                </Link>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        );
+  render() { //Render component
+    if (localStorage.getItem("token")) {
+      return <Redirect to="/" />;
     }
+    return (
+    <div>
+      <br/>
+      <div className="container">
+        <form onSubmit={this.login} className="form-signin">
+          <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+          <div className="row">
+            <div className="col">
+              <input
+                type="text"
+                name="user_id"
+                onChange={this.handleFormChange}
+                placeholder="Enter Username"
+              />
+              <input
+                type="password"
+                name="user_password"
+                onChange={this.handleFormChange}
+                placeholder="Enter Password"
+              />
+              <input type="submit" value="Login" />
+              <Link to="/register" >
+              <button>
+                Register Here if You Don't Have an Account!
+              </button>
+              </Link>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+    );
+  }
 }
 export default Login;
