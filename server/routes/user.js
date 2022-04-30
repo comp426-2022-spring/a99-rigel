77b691f01f33047ab9030ae2d1160668ca9351de
Max Hu <max@12450.top>
2022-04-29 20:39:14 -0400

    Backend:Add: hashed password

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,13 +1,4 @@
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken'
-
-// function to creat jwt
-const maxAge = 3 * 24 * 60 * 60;
-const createToken = (id) => {
-  return jwt.sign({ id }, 'secret', {
-    expiresIn: maxAge
-  })
-}
 
 // controller actions
 export function register_post(req, res){
@@ -23,32 +14,34 @@ export function register_post(req, res){
         if (data) {
             res.status(200).json({message: "Error: your account has already been registered"});
         } else {
-            add_user_helper(req, res, db);
+            bcrypt.genSalt(10, function(err, salt) {
+                bcrypt.hash(password, salt, function(err, hash) {
+                    add_user_helper(req, res, db, hash);
+                });
+            });
         }
     });
 }
 
-function add_user_helper(req, res, db) {
+function add_user_helper(req, res, db, password) {
     const data = {
         user_name: req.body.user_name,
         user_email: req.body.user_email,
-        user_password : req.body.user_password,
+        user_password : password,
         email_verified: true,
         user_info: req.body.user_info,
         user_intro: req.body.user_intro,
     };
-    
     db.insertOne(data, (err, resdb) => {
         if (err) res.send({
             status: 'error',
             debug: resdb
         });
-        res.status(201).json({ user: user.user_id });
+        res.status(201).json({ status: 'success', msg: { user: resdb.insertedId }});
     });
 }
 
 export async function login_post(req, res) {
-    // console.log(JSON.stringify(req))
     const User = req.app.get('db').collection('user');
     const name = req.body.user_name;
     const password = req.body.user_password;
@@ -58,16 +51,14 @@ export async function login_post(req, res) {
 }
 
 function login(user, password, res) {
-    console.log(user);
     if (user) {
-        const auth = password === user.user_password;
-        if (auth) {
-            const token = createToken(user.user_id);
-            res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
-            res.status(200).json({ user: user.user_id });
-        } else {
-            res.status(510).json({message: "Nope! Wrong email or password!"});
-        }
+        bcrypt.compare(password, user.user_password, (err, auth) => {
+            if (auth) {
+                res.status(200).json({ user: user.user_id });
+            } else {
+                res.status(510).json({message: "Nope! Wrong email or password!"});
+            }
+        });
     } else {
         res.status(510).json({message: "Nope! Wrong email or password!"})
     }
